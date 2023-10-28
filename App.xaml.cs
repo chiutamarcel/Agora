3064f1f203cfbd34ce42f088134651dc599282a9
Chiuta Mihai Marcel <chiuta.marcel@gmail.com>
2023-10-28 13:46:36 +0300

    Structured project into MVVM

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Agora.MVVM.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,27 @@ namespace Agora
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<MainWindow>(provider => new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainViewModel>()
+            });
+
+            services.AddSingleton<MainViewModel>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
