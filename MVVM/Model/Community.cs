a62338bf68c83f2c600e6e257bc187419a1ae0e4
Barar Daniel <danielbarar77@gmail.com>
2023-11-08 22:51:30 +0200

    added the following classes: IEntity, Entity, IPost, Post, ICommunity, Community

---
+++ MVVM/Model/Community.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agora.MVVM.Model
+{
+    internal class Community : Entity, ICommunity
+    {
+        public Community(int id) : base(id)
+        {
+        }
+
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int OwnerID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int CategoryID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
