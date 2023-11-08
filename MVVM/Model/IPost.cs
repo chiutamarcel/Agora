80662268c1d1a9a410c0431022b5f5e53e3ba5f8
Barar Daniel <danielbarar77@gmail.com>
2023-11-08 22:51:30 +0200

    added the following classes: IEntity, Entity, IPost, Post, ICommunity, Community

---
+++ MVVM/Model/IPost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Agora.MVVM.Model
+{
+    enum PostType
+    {
+        Article,
+        News,
+        Review
+    }
+    internal interface IPost
+    {
+        PostType Type { get; set; }
+        string Title { get; set; }
+        string Content { get; set; }
+        DateTime Date { get; set; }
+        int AuthorID { get; set; }
+        int CategoryID { get; set; }
+
+        void Save();
+        void Delete();                
+    }
+}
---
+++ MVVM/Model/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agora.MVVM.Model
+{
+    internal class Post : Entity, IPost
+    {
+        public PostType Type { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public int AuthorID { get; set; }
+        public int CategoryID { get; set; }
+
+        public Post(int id) : base(id)
+        {
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
