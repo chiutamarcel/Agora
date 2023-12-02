aca2cc31cf35275bdd0894e405b5df06d51b4fe9
Barar Daniel <danielbarar77@gmail.com>
2023-12-02 17:02:20 +0200

    Added RegisterView

--- MVVM/View/MainView.xaml.cs
+++ MVVM/View/MainView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Agora.MVVM.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,14 @@ namespace Agora
         public MainWindow()
         {
             InitializeComponent();
+            this.Show();
+
+            //load register view after MainWindow was successfuly loaded
+            if (this != null)
+            {
+                RegisterView registerView = new RegisterView();
+                registerView.Show();
+            }
         }
     }
 }
---
+++ MVVM/View/RegisterView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace Agora.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for RegisterView.xaml
+    /// </summary>
+    public partial class RegisterView : Window
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }        
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper _paletteHelper = new PaletteHelper();
+        private void themeToggle_Click(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = _paletteHelper.GetTheme();
+
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                theme.SetBaseTheme(Theme.Dark);
+                IsDarkTheme = true;
+            }
+            _paletteHelper.SetTheme(theme);
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+    }
+}
